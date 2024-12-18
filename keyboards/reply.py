4c42ca70f0e43c37f8e5b15ff0f2f8512b477ed6
Evgeny <dartcyberart@gmail.com>
2024-12-18 19:22:47 +0300

    -||-

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -22,7 +22,7 @@ add_new_casting = ReplyKeyboardMarkup(keyboard=[
 
 pay_choice = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Подписка на месяц дней - 599₽')],
-    [KeyboardButton(text='Подписка на 3 месяца - 1370₽')]
+    [KeyboardButton(text='Подписка на 3 месяца - 1370₽ (-24%)')]
 ], one_time_keyboard=True, resize_keyboard=True)
 
 
